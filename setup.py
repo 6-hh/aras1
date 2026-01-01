3bcc71e7739606ccb9fd9865a003b175a8828f89
𓏺 𝗮𝗥𝗮𝗦 <hghjvvv6@gmail.com>
2026-01-01 23:34:37 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aras",
+    version="1.0.0",
+    author="W4_M4",
+    author_email="",
+    description="ARAS_Logo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/6-hh/aras",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
