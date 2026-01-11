9b90a44ac8c8d4c5d9e1be1bdba569c289cdea52
𓏺 𝗮𝗥𝗮𝗦 <hghjvvv6@gmail.com>
2026-01-11 19:56:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="aras1",
-    version="1.0.1",
+    version="1.0.2", 
     author="W4_M4",
     author_email="",
-    description="ARAS_Logo",
+    description="Instagram, TikTok, Yopmail",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/6-hh/aras1",
@@ -17,6 +17,15 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Natural Language :: Arabic",
     ],
     python_requires=">=3.6",
-)
+    keywords="aras Logo 2",
+    project_urls={
+        "Bug Reports": "https://github.com/6-hh/aras1/issues",
+        "Source": "https://github.com/6-hh/aras1",
+        "Telegram": "https://t.me/W4_M4",
+    },
